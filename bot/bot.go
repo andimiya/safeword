d33acd2009e7f0f0372afa335514350634412ad3
Jon Borgonia <jon@gomagames.com>
2017-07-29 15:56:36 -1000

    gorilla server with GET /start endpoint

--- bot/bot.go
+++ bot/bot.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
+	"log"
+	"net/http"
 	"time"
 )
 
 func main() {
-
 	r := raspi.NewAdaptor()
 	led := gpio.NewLedDriver(r, "31")
 
+	router := mux.NewRouter()
+	router.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		led.On()
+
+		fmt.Println("hit /start")
+		fmt.Fprintln(w, "ok")
+
+		time.Sleep(time.Second * 2)
+		led.Off()
+
+	}).Methods("GET")
+
 	work := func() {
-		gobot.Every(1*time.Second, func() {
-			led.Toggle()
-		})
+
+		fmt.Println("listening on port 8080")
+		log.Fatal(http.ListenAndServe(":8080", router))
 	}
 
 	robot := gobot.NewRobot("SafeWordBot",
