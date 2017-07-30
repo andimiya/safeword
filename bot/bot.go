99305bd3806054151ecc164c5da892dda5e31cf5
Jon Borgonia <jon@gomagames.com>
2017-07-29 19:19:16 -1000

    gobot: command to record on GET /start

--- bot/bot.go
+++ bot/bot.go
@@ -1,34 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
+
+	"bytes"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"os/exec"
 )
 
+var r *raspi.Adaptor
+var led *gpio.LedDriver
+
 func main() {
-	r := raspi.NewAdaptor()
-	led := gpio.NewLedDriver(r, "31")
+	r = raspi.NewAdaptor()
+	led = gpio.NewLedDriver(r, "31")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-		led.On()
-
-		fmt.Println("hit /start")
-		fmt.Fprintln(w, "ok")
-
-		time.Sleep(time.Second * 2)
-		led.Off()
-
-	}).Methods("GET")
+	router.HandleFunc("/start", handleStart).Methods("GET")
 
 	work := func() {
-
+		led.Off()
 		fmt.Println("listening on port 8080")
 		log.Fatal(http.ListenAndServe(":8080", router))
 	}
@@ -41,4 +37,26 @@ func main() {
 
 	robot.Start()
 
+}
+
+func handleStart(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hit /start")
+	fmt.Fprintln(w, "ok")
+
+	led.On()
+	startRecordingCmd()
+	led.Off()
+
+}
+
+func startRecordingCmd() {
+	cmd := exec.Command("sh", "/home/pi/scripts/record.sh")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	fmt.Printf("RECORDING RESULTS: %q\n", out.String())
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
