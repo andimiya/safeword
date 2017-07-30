7c5ab8b6950b5ee08b8b4c5d91e8033303e9db90
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-29 15:16:20 -1000

    Scripts that are on the raspberry pi. These push up to S3

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -2,7 +2,8 @@
 
 mkdir -p cam
 
-ssh pi@10.10.110.206 fswebcam -d /dev/video* --png 0 -r 960x720 --subtitle 'SafeWord' --save ./cam/`date -u +%Y-%m-%d_%H-%M-%S`.png
+ssh -p 10954 pi@0.tcp.ngrok.io fswebcam -d /dev/video* --png 0 -r 960x720 --subtitle 'SafeWord' --save ./cam/`date -u +%Y-%m-%d_%H-%M-%S`.png
 
-rsync -urltv -e ssh pi@10.10.110.206:~/cam/ ./cam/
+rsync -urltv -e "ssh -p 10954" pi@0.tcp.ngrok.io:~/cam/ ./cam/
 
+aws s3 sync ./cam s3://safeword-storage/cam
