31276cce0375e8a77f84f0489a3faf7437ebeea4
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-29 18:26:44 -1000

    Updated scripts

--- scripts/record.sh
+++ scripts/record.sh
@@ -9,6 +9,6 @@ mkdir -p videos
 avconv -f video4linux2 -r 30 -s 640x480 -i /dev/video* \
   -f alsa -i plughw:U0x46d0x81b,0 \
   -ar 22050 -ab 64k -strict experimental -acodec aac \
-  -frames:v $FRAMES -vcodec mpeg4 -y ./videos/`date -u +%Y-%m-%d_%H-%M-%S`.mp4
+  -frames:v $FRAMES -vcodec mpeg4 -y ~/videos/`date -u +%Y-%m-%d_%H-%M-%S`.mp4
 
-aws s3 sync ./videos s3://safeword-storage/videos
+aws s3 sync ~/videos s3://safeword-storage/videos
--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -2,8 +2,6 @@
 
 mkdir -p cam
 
-ssh -p 10954 pi@0.tcp.ngrok.io fswebcam -d /dev/video* --png 0 -r 960x720 --subtitle 'SafeWord' --save ./cam/`date -u +%Y-%m-%d_%H-%M-%S`.png
+fswebcam -d /dev/video* --png 0 -r 960x720 --subtitle 'SafeWord' --save ~/cam/`date -u +%Y-%m-%d_%H-%M-%S`.png
 
-rsync -urltv -e "ssh -p 10954" pi@0.tcp.ngrok.io:~/cam/ ./cam/
-
-aws s3 sync ./cam s3://safeword-storage/cam
+aws s3 sync ~/cam s3://safeword-storage/cam
